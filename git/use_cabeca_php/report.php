653e7873b47c863d1dde0e2559a6397794b97858
Gildasio Bizerra Brito <gildasio.ariquemes@hotmail.com>
2025-02-04 08:47:55 -0400

    "Projetos|_estudos"

--- git/use_cabeca_php/report.php
+++
@@ -1,7 +0,0 @@
-<?php 
-
-    echo "<strong>Nome: </strong>". $nome = $_POST['nome'] ."<br>";
-    echo "<strong>idade:</strong> " . $idade = $_POST['idade']."<br>";;
-    echo "<strong>email:</strong>" . $email = $_POST['email']."<br>";;
-
-?>
